0f259f3ea6b405302128b6f59e86924392c1452e
kdi6033 <kdi6033@gmail.com>
2021-08-18 11:18:18 +0900

    센서 추가

--- 11-4-1 [sensecube] KSM-8900 Local/down-local-04/handleHttp.ino
+++ 11-4-1 [sensecube] KSM-8900 Local/down-local-04/handleHttp.ino
@@ -81,18 +81,22 @@ char Head[] PROGMEM = R"=====(
   </style>
 )=====";
 
-char RootScript[] PROGMEM = R"=====(
+// 홈화면에서 실시간 계측값 표시
+char ScriptRoot[] PROGMEM = R"=====(
   <script>
     var Socket;
     function init() {
       Socket = new WebSocket('ws://' + window.location.hostname + ':81/');
       Socket.onmessage = function(event){
         var data = JSON.parse(event.data);
-        //console.log(data.ec);
+        console.log(data.temp);
         document.getElementById("mo").innerHTML = data.mo;
         document.getElementById("tem").innerHTML = data.tem;
         document.getElementById("ec").innerHTML = data.ec;
       }
+    }
+    function sendAct(valueIn){
+      Socket.send(valueIn);
     }
     function openNav() {
       document.getElementById("mySidenav").style.width = "150px"; 
@@ -105,8 +109,7 @@ char RootScript[] PROGMEM = R"=====(
 <body onload="javascript:init()">
 )=====";
 
-
-char HeadScript[] PROGMEM = R"=====(
+char ScriptHead[] PROGMEM = R"=====(
   <script>
     var Socket;
     function init() {
@@ -163,16 +166,16 @@ char Menu[] PROGMEM = R"=====(
   <a href='/wifi'>와이파이설정</a>
   <a href='/config'>환경설정</a>
   <a href='/manual'>메뉴얼</a>
-  <a href='http://i2r.link'>김동일홈피</a>
+  <a href='http://i2r.link' target='_blank'>김동일홈피</a>
   </div>
   <span style='font-size:30px;cursor:pointer' onclick='openNav()'>&#9776; </span>
   메뉴열기
 )=====";
 
 char Download[] PROGMEM = R"=====(
-  <br><br>
-  새로운 기기가 계속 추가되고 있습니다.
-  추가된 기기를 다운로드 받으려면 펌웨어 업그레이드를 하시고 거기서 원하는 기기의 펌웨어를 선택해서 다운로드 받으세요.
+  <br><br>다운로드<br>
+  새로운 기기를 다운로드 받으려면 [최신펌웨어 업그레이드]를 하면 기기 리스트가 보입니다.<br> 
+  기기선택 후 업그레이드 하세요.<br>
   <div> <button id="onButton" class='button button-box' onclick="sendAct('#'+'{\'act\':1}');">기기선택 펌웨어 다운로드</button> </div>  
   <hr>
 )=====";
@@ -191,7 +194,7 @@ char Tail[] PROGMEM = R"=====(
 void handleRoot() {
   String s;
   s=FPSTR(Head);
-  s+=FPSTR(RootScript);
+  s+=FPSTR(ScriptRoot);
   s+=FPSTR(Menu);
   s+=FPSTR(Body);
   s+=FPSTR(Tail);
@@ -214,7 +217,6 @@ void handleOn() {
     Serial.println(ipMqtt);
     Serial.println(timeMqtt);
     Serial.println(email);
-    tickerMqtt.attach(timeMqtt, tickMqtt); 
     saveConfig();
   }
   GoHome();
@@ -266,6 +268,7 @@ String sensWifi(int in) {
     return "9776";
 }
 
+
 void handleWifi() {
   String s=""; 
   String s1= String(ssid);
@@ -305,7 +308,7 @@ void handleWifi() {
     s+="</tr>";
   String sOut;
   sOut=FPSTR(Head);
-  sOut+=FPSTR(HeadScript);
+  sOut+=FPSTR(ScriptHead);
   sOut+=FPSTR(Menu);
   sOut+=s;
   sOut+=FPSTR(Tail);
@@ -341,8 +344,10 @@ void handleScan() {
 
 void handleConfig() {
   String s;
-  s="<br><br>현장에서 mqtt 통신을 사용하지 않고 웹페이지 모니터링만 하려면 입력하지 않아도 됩니다.<br>";
-  s+="<br> 아마존 서버 http://i2r.link 접속하려면 회원가입 한 email을 입력하세요.<br>";
+  s="<br><br>현장에서 MQTT 통신을 사용하지 않고 웹페이지 모니터링만 하려면 <br>";
+  s+="<br> 와이파이-485 보드의 리셋키를 누르면 MQTT서버 주소가 지워지고 센서 값만 읽을수 있습니다.<br>";
+  s+="<br> MQTT서버 IP 주소는 서버에서 통신으로 관리되어 입력하지 않아도 됩니다.<br>";
+
   s+="<form action='/on'>";
   s+="<input type='hidden' name='act' value='2'>";
   s+="<table>";
@@ -350,26 +355,17 @@ void handleConfig() {
       s+="<th>mqtt서버 IP</th>";
       s+="<th><input type='text' value='"+(String)ipMqtt+"' name='ipMqtt'/></th>";
     s+="</tr>";
-
-    s+="<tr>";
-      s+="<th>mqtt 통신속도(초)</th>";
-      s+="<th><input type='number' id='timeMqtt' name='timeMqtt' min='1' value='"+(String)timeMqtt+"'/></th>";
-    s+="</tr>";
-    
-    s+="<tr>";
-      s+="<th>email</th>";
-      s+="<th><input type='text' value='"+(String)email+"' name='email'/></th>";
-    s+="</tr>";
     s+="<tr>";
       s+="<th></th>";
       s+="<th><input type='submit' value='    저  장    ' style='background-color:#ff8000;color: white;border: none;padding: 6px 15px;'/></th>";
     s+="</tr>";
+
   s+="</table>";
   s+="</form>";
 
   String sOut;
   sOut=FPSTR(Head);
-  sOut+=FPSTR(HeadScript);
+  sOut+=FPSTR(ScriptHead);
   sOut+=FPSTR(Menu);
   sOut+=s;
   sOut+=FPSTR(Tail);
@@ -379,12 +375,8 @@ void handleConfig() {
 void handleDownload() {
   String s;
   s=FPSTR(Head);
-  s+=FPSTR(HeadScript);
+  s+=FPSTR(ScriptHead);
   s+=FPSTR(Menu);
-  if(FirmwareVer != FirmwareVerServer) {
-    s+="<br><br>새로운 기기가 추가 되었습니다. 펌웨어 업그레이드 하세요.<br>";
-    s+="새로운 버젼"+FirmwareVerServer+"&emsp; 보드버젼"+FirmwareVer;
-  }
   s+=FPSTR(Download);
   s+=FPSTR(Tail);
   server.send(200, "text/html", s);
@@ -393,7 +385,7 @@ void handleDownload() {
 void handleManual() {
   String s;
   s=FPSTR(Head);
-  s+=FPSTR(HeadScript);
+  s+=FPSTR(ScriptHead);
   s+=FPSTR(Menu);
   s+=FPSTR(Manual);
   s+=FPSTR(Tail);
